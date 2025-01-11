16652f466efd5f136aa6ed17b4e79a8ffb3f7a51
Konrad <konrad.sygut@sap.com>
2025-01-11 21:44:23 +0100

    integration with azure

--- src/test/java/com/example/rentalservices/PeselHandlerTests.java
+++ src/test/java/com/example/rentalservices/PeselHandlerTests.java
@@ -1,5 +1,6 @@
 package com.example.rentalservices;
 
+import com.example.rentalservices.exception.ValidationException;
 import com.example.rentalservices.validator.PeselHandler;
 import org.junit.jupiter.api.Test;
 
@@ -11,26 +12,46 @@ public class PeselHandlerTests {
     @Test
     void testIsPeselValid_validAdultPesel() {
         //pesel osoby pelnoletniej (ur. 22-02-1990)
-        String validPesel = "90022212345";
+        String validPesel = "20011012345";
         assertTrue(peselHandler.isPeselValid(validPesel), "PESEL powinien być poprawny i wskazywać na osobę pełnoletnią.");
     }
     @Test
     void testIsPeselValid_invalidLength() {
         //niepoprawny pesel wzgledem dlugosci znakow
         String invalidPesel = "1234567890";
-        assertFalse(peselHandler.isPeselValid(invalidPesel), "PESEL o niepełnej długości powinien być niepoprawny.");
+        ValidationException exception = assertThrows(
+                ValidationException.class,
+                () -> peselHandler.isPeselValid(invalidPesel),
+                "Invalid pesel number"
+        );
+
+        assertEquals("Invalid pesel number", exception.getMessage(), "Komunikat wyjątku niezgodny z oczekiwanym.");
     }
     @Test
     void testIsPeselValid_invalidCharacters() {
         // pesel z literami alfabetu
         String invalidPesel = "90022A12345";
-        assertFalse(peselHandler.isPeselValid(invalidPesel), "PESEL zawierający litery powinien być niepoprawny.");
+        ValidationException exception = assertThrows(
+                ValidationException.class,
+                () -> peselHandler.isPeselValid(invalidPesel),
+                "Invalid pesel number"
+        );
+
+        assertEquals("Invalid pesel number", exception.getMessage(), "Komunikat wyjątku niezgodny z oczekiwanym.");
+
     }
     @Test
     void testIsPeselValid_underagePesel() {
         // pesel osoby niepełnoletniej (ur. 22-02-2010)
         String underagePesel = "10222212345";
-        assertFalse(peselHandler.isPeselValid(underagePesel), "PESEL osoby niepełnoletniej powinien być niepoprawny.");
+        ValidationException exception = assertThrows(
+                ValidationException.class,
+                () -> peselHandler.isPeselValid(underagePesel),
+                "Cannot create account. You are underage"
+        );
+
+        assertEquals("Cannot create account. You are underage", exception.getMessage(), "Komunikat wyjątku niezgodny z oczekiwanym.");
+
     }
 
 }
